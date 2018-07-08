e65478d1cb7fada446dd01baec191db4b753b8a3
Forrest Feng <changzheng.feng@carestream.com>
2018-07-08 22:27:03 +0800

    Set async_mode to None to let flask-sockect io pick the right one. ?? How apache2 to pick it ???

--- file_server.py
+++ file_server.py
@@ -184,7 +184,7 @@ app.add_url_rule('/<path:p>', view_func=path_view )
 
 # Part of socket io
 from flask_socketio import SocketIO, emit
-async_mode = 'eventlet'
+async_mode = None
 socketio = None
 socketio = SocketIO(app, async_mode=async_mode)    
 app.config['SECRET_KEY'] = 'secret!'
